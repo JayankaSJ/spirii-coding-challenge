494b49d29a04a8f1d8fe3774c78b837709a7b98f
Jayanka Sampath <jaynksj@gmail.com>
2025-02-15 13:57:54 +0100

    unwanted files removed

---
+++ README.md
@@ -0,0 +1,9 @@
+## Available Scripts
+
+`cd frontend && npm install`
+`cd backend && npm install`
+
+## Start
+
+`cd frontend && npm start`
+`cd backend && npm start`
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DragonController } from './modules/dragon/dragon.controller';
 import { DragonService } from './modules/dragon/dragon.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, DragonController],
-  providers: [AppService, DragonService],
+  controllers: [DragonController],
+  providers: [DragonService],
 })
 export class AppModule {}
