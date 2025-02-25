52c9c45a47c09eb399ad2ed43b304aed81ff464c
Jayanka Sampath <jaynksj@gmail.com>
2025-02-25 21:41:12 +0100

    frontend test suit configured

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-
-import "./App.css";
 import { FightArea } from "modules/fight-area/fight-area";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FightingContextProvider } from "contexts/fighting-context";
 import { DragonsContextProvider } from "contexts/dragons-context";
+import "./App.css";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +15,9 @@ function App() {
         <DragonsContextProvider>
           <div className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12">
             <div className="flex flex-col gap-4 justify-center items-center p-10">
-              <span className="text-3xl">Dragon Fight</span>
+              <span className="text-3xl" data-testid="header-text">
+                Dragon Fight
+              </span>
               <FightingContextProvider>
                 <FightArea />
               </FightingContextProvider>
